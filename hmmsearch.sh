f59b9f33d03f8279034bad59202c3fab366248aa
SongCanChen11 <97407459+SongCanChen11@users.noreply.github.com>
2025-04-28 10:47:01 +0800

    Add files via upload

---
+++ hmmsearch.sh
@@ -0,0 +1,152 @@
+## Extract homologs of ttrA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_ttrA.ga.out --noali --cut_ga --cpu 20 HMMs/ttrA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of shyB from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_shyB.ga.out --noali --cut_ga --cpu 20 HMMs/shyB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of phsA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_phsA_psrA.ga.out --noali --cut_ga --cpu 20 HMMs/phsA_psrA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of otr from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_otr.ga.out --noali --cut_ga --cpu 20 HMMs/otr.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of npsr from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_npsr.ga.out --noali --cut_ga --cpu 20 HMMs/npsr.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of mccA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_mccA.ga.out --noali --cut_ga --cpu 20 HMMs/mccA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dsrA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dsrA.ga.out --noali --cut_ga --cpu 20 HMMs/dsrA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_reductive_dsrA.ga.out --noali --cut_ga --cpu 20 HMMs/reductive_dsrA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_oxidative_dsrA.ga.out --noali --cut_ga --cpu 20 HMMs/oxidative_dsrA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_archaeal_dsrA.ga.out --noali --cut_ga --cpu 20 HMMs/archaeal_dsrA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of aprA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_aprA.ga.out --noali --cut_ga --cpu 20 HMMs/aprA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dsrB from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dsrB.ga.out --noali --cut_ga --cpu 20 HMMs/dsrB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_reductive_dsrB.ga.out --noali --cut_ga --cpu 20 HMMs/reductive_dsrB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_oxidative_dsrB.ga.out --noali --cut_ga --cpu 20 HMMs/oxidative_dsrB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_archaeal_dsrB.ga.out --noali --cut_ga --cpu 20 HMMs/archaeal_dsrB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of asrA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_asrA.ga.out --noali --cut_ga --cpu 20 HMMs/asrA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of sudA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_sudA.ga.out --noali --cut_ga --cpu 20 HMMs/sudA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of fccB from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_fccB.ga.out --noali --cut_ga --cpu 20 HMMs/fccB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of tsdA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_tsdA.ga.out --noali --cut_ga --cpu 20 HMMs/tsdA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of sqr from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_sqrI.ga.out --noali --cut_ga --cpu 20 HMMs/sqrI.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sqrII.ga.out --noali --cut_ga --cpu 20 HMMs/sqrII.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sqrIII.ga.out --noali --cut_ga --cpu 20 HMMs/sqrIII.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sqrIV.ga.out --noali --cut_ga --cpu 20 HMMs/sqrIV.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sqrV.ga.out --noali --cut_ga --cpu 20 HMMs/sqrV.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sqrVI.ga.out --noali --cut_ga --cpu 20 HMMs/sqrVI.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of soxB from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_soxB.ga.out --noali --cut_ga --cpu 20 HMMs/soxB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_putative_soxB.ga.out --noali --cut_ga --cpu 20 HMMs/putative_soxB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of sorT from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_sorT1.ga.out --noali --cut_ga --cpu 20 HMMs/sorT1.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sorT2.ga.out --noali --cut_ga --cpu 20 HMMs/sorT2.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of sorA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_sorA1.ga.out --noali --cut_ga --cpu 20 HMMs/sorA1.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sorA2.ga.out --noali --cut_ga --cpu 20 HMMs/sorA2.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sorA3.ga.out --noali --cut_ga --cpu 20 HMMs/sorA3.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of soeA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_soeA_sreA.ga.out --noali --cut_ga --cpu 20 HMMs/soeA_sreA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of sdo from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_sdoS1.ga.out --noali --cut_ga --cpu 20 HMMs/sdoS1.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sdoS2.ga.out --noali --cut_ga --cpu 20 HMMs/sdoS2.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_ETHE1.ga.out --noali --cut_ga --cpu 20 HMMs/ETHE1.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_sdoA.ga.out --noali --cut_ga --cpu 20 HMMs/sdoA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of doxD from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_doxD.ga.out --noali --cut_ga --cpu 20 HMMs/doxD.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of doxDA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_doxDA.ga.out --noali --cut_ga --cpu 20 HMMs/doxDA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of yihQ from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_yihQ.ga.out --noali --cut_ga --cpu 20 HMMs/yihQ.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of tmm from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_tmm.ga.out --noali --cut_ga --cpu 20 HMMs/tmm.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of ssuD from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_ssuD.ga.out --noali --cut_ga --cpu 20 HMMs/ssuD.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of sfnG from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_sfnG.ga.out --noali --cut_ga --cpu 20 HMMs/sfnG.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of mtoX from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_mtoX.ga.out --noali --cut_ga --cpu 20 HMMs/mtoX.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of msmA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_msmA.ga.out --noali --cut_ga --cpu 20 HMMs/msmA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of mddA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_mddA1.ga.out --noali --cut_ga --cpu 20 HMMs/mddA1.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_mddA2.ga.out --noali --cut_ga --cpu 20 HMMs/mddA2.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_mddA3.ga.out --noali --cut_ga --cpu 20 HMMs/mddA3.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_mddA4.ga.out --noali --cut_ga --cpu 20 HMMs/mddA4.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_mddA5.ga.out --noali --cut_ga --cpu 20 HMMs/mddA5.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dsyB from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dsyB.ga.out --noali --cut_ga --cpu 20 HMMs/dsyB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dsoB from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dsoB.ga.out --noali --cut_ga --cpu 20 HMMs/dsoB.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dmsA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_DMSOR.ga.out --noali --cut_ga --cpu 20 HMMs/DMSOR.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_bacterial_dmsA.ga.out --noali --cut_ga --cpu 20 HMMs/bacterial_dmsA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_archaeal_dmsA.ga.out --noali --cut_ga --cpu 20 HMMs/archaeal_dmsA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_bacterial_dmsA1.ga.out --noali --cut_ga --cpu 20 HMMs/bacterial_dmsA1.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dmoA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dmoA.ga.out --noali --cut_ga --cpu 20 HMMs/dmoA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dddY from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dddY.ga.out --noali --cut_ga --cpu 20 HMMs/dddY.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dddX from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dddX.ga.out --noali --cut_ga --cpu 20 HMMs/dddX.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+hmmsearch --tblout Res/01_hmm_dddX_1.ga.out --noali --cut_ga --cpu 20 HMMs/dddX_1.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dddQ from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dddQ.ga.out --noali --cut_ga --cpu 20 HMMs/dddQ.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dddP from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dddP.ga.out --noali --cut_ga --cpu 20 HMMs/dddP.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dddL from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dddL.ga.out --noali --cut_ga --cpu 20 HMMs/dddL.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dddW from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dddW.ga.out --noali --cut_ga --cpu 20 HMMs/dddW.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of dddD from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_dddD.ga.out --noali --cut_ga --cpu 20 HMMs/dddD.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of mtsA from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_mtsA.ga.out --noali --cut_ga --cpu 20 HMMs/mtsA.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of sHdrB1 from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res//01_hmm_sHdrB1.ga.out --noali --cut_ga --cpu 20 HMMs//sHdrB1.hmm.curated /mnt/volume/Tools/GTDB/r95/protein_faa_reps/GTDB_r95_demo.faa &>/dev/null &
+
+## Extract homologs of mmtN from GTDB r95 with the --cut_ga option
+hmmsearch --tblout Res/01_hmm_mmtN.ga.out --noali --cut_ga --cpu 20 HMMs/mmtN.hmm.curated GTDB_r95_demo.faa &>/dev/null &
+
